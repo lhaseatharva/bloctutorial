ad1b5aa45afbb0b7975f3b5ccca827368c44376b
atharva <atharvalhase99@gmail.com>
2024-01-13 19:30:44 +0530

    Initial Commit

---
+++ lib/features/home/ui/home.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutterbloctutorial/features/cart/bloc/ui/cart.dart';
+import 'package:flutterbloctutorial/features/home/bloc/home_bloc.dart';
+import 'package:flutterbloctutorial/features/wishlist/bloc/ui/wishlist.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  void initState() {
+    homeBloc.add(HomeInitialEvent());
+    super.initState();
+  }
+
+  final HomeBloc homeBloc = HomeBloc();
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<HomeBloc, HomeState>(
+      bloc: homeBloc,
+      listenWhen: (previous, current) => current is HomeActionState,
+      buildWhen: (previous, current) => current is HomeActionState,
+      listener: (context, state) {
+        if (state is HomeNavigateToCartPageActionState) {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => Cart()));
+        } else if (state is HomeNavigateToWishlistPageActionState) {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => Wishlist()));
+        }
+      },
+      builder: (context, state) {
+        switch (state.runtimeType) {
+          case HomeLoadingState:
+            return Scaffold(
+                body: Center(
+              child: CircularProgressIndicator(),
+            ));
+
+          case HomeLoadedSuccessState:
+            return Scaffold(
+              appBar: AppBar(
+                backgroundColor: Colors.teal,
+                title: const Text(
+                  'Grocery App',
+                  style: TextStyle(color: Colors.white),
+                ),
+                actions: [
+                  IconButton(
+                      onPressed: () {
+                        homeBloc.add(HomeWishlistButtonNavigateEvent());
+                      },
+                      icon: Icon(Icons.favorite_border, color: Colors.white)),
+                  IconButton(
+                      onPressed: () {
+                        homeBloc.add(HomeCartButtonNavigateEvent());
+                      },
+                      icon: Icon(Icons.shopping_bag_outlined,
+                          color: Colors.white))
+                ],
+              ),
+            );
+
+          case HomeErrorState:
+            return Scaffold(
+              body: Center(child: Text('Error')),
+            );
+
+          default:
+            return SizedBox();
+        }
+      },
+    );
+  }
+}
+
+
+// return 
