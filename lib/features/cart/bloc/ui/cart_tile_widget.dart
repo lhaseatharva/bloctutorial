e06374552c5f817918991b3ca3a10a91b47535eb
atharva <atharvalhase99@gmail.com>
2024-01-14 17:06:30 +0530

    Bug fixes

--- lib/features/cart/bloc/ui/cart_tile_widget.dart
+++ lib/features/cart/bloc/ui/cart_tile_widget.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutterbloctutorial/features/cart/bloc/cart_bloc.dart';
 import 'package:flutterbloctutorial/features/home/models/home_product_data_model.dart';
 
